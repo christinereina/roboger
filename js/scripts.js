9e219ad4f8360497526418e8318811b8dbceed1d
blacchoy@gmail.com <blacchoy@gmail.com>
2020-06-12 15:12:05 -0700

    added function for error handling - first spec passed

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,21 @@
-
+function handleErrors(number) {
+  const regex = /^[0-9]*$/
+  const isValid = regex.test(number);
+  if (!isValid) {
+      alert("Sorry I can only talk in numbers. Try again.");
+  }
+  return number;
+}
 
 // user interface logic below
 
 $(document).ready(function() {
   $("#form").submit(function(event) {
       event.preventDefault();
-    const userInput = parseInt($("input#user").val());
+    const userInput = $("input#user").val();
     console.log(userInput);
+    const noErrors = handleErrors(userInput);  
+    console.log(noErrors);
 
   });
 });
