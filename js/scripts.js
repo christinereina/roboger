5161f90433a962e19c37422de8fddd1ef594c769
blacchoy@gmail.com <blacchoy@gmail.com>
2020-06-12 15:25:12 -0700

    add robot function - all specs passing

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+//business logic 
+
 function handleErrors(number) {
   const regex = /^[0-9]*$/
   const isValid = regex.test(number);
@@ -7,15 +9,35 @@ function handleErrors(number) {
   return number;
 }
 
-// user interface logic below
+function robot(userInput) {
+
+  const messages = ["Beep", "Boop", "Wont you be my neighbor"];
+  const list = [];
+  
+  for (var i = 1; i <= userInput; i++) {
+    let numbers = i.toString().split("");
+    if (numbers.includes("1")) {
+      list.push(messages[0]);
+    } else if (numbers.includes("2")) {
+      list.push(messages[1]);
+    } else if (numbers.includes("3")) {
+      list.push(messages[2]);
+    } else {
+      list.push(i.toString());
+    }
+  }
+  return list.join(", ");
+}
+
+// user interface logic 
 
 $(document).ready(function() {
   $("#form").submit(function(event) {
       event.preventDefault();
     const userInput = $("input#user").val();
-    console.log(userInput);
     const noErrors = handleErrors(userInput);  
-    console.log(noErrors);
+    const robotOutput = robot(userInput);
+    console.log(robotOutput);
 
   });
 });
